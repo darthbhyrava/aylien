8fed52b6ef8d78d617adaa21bc5aa3aa71ba8cf2
darthbhyrava <hbhyrava@gmail.com>
2018-02-26 12:27:49 +0530

    Less effective than Watson.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # aylien
-Testing out Aylien's Text API
+Testing out Aylien's Text API for sentiment analysis of tweets.
+
+#### Overall Sentiment Analysis
+Ran API calls for _21Feb\_Neg.xlsx_ (337 false postives: our sent>0, manual=Negative) and _21Feb\_Pos.xlsx_ (81 false negatives: our sent<0, manual=Positive) with accuracies of 39.4% and 9.8% respectively. IBM Watson's API is better (and more expensive).
+
+#### Aspect-Based Sentiment Analysis
+
---
+++ sent.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import openpyxl
+import time
+from aylienapiclient import textapi
+
+client = textapi.Client("7a228a93", "bcd1bca1d5c975ede7d49d1b9ce611b0")
+
+wb = openpyxl.load_workbook('21Feb_Pos.xlsx')
+ws = wb.active
+pred_sent_col = ws['L']
+man_sent_col = ws['Q']
+text_col = ws['U']
+rows = len(pred_sent_col)
+
+correct_p = 0.0
+all_rows = []
+for row in range(1,rows):
+    print row
+    time.sleep(0.1)
+    towrite = {}
+    pred_sent = pred_sent_col[row].value
+    man_sent = man_sent_col[row].value
+    text = text_col[row].value.replace(';','')
+    try:
+    	response = client.Sentiment({'text':text, 'mode':'tweet'})
+    except:
+    	continue
+    aylien_sent = response['polarity']
+    aylien_conf = response['polarity_confidence']
+    towrite['Manual Sentiment'] = man_sent
+    towrite['Aylien_Label'] = aylien_sent
+    towrite['Text'] = text.encode('utf-8')
+    if aylien_sent == 'positive':
+        correct_p+=1.0
+    towrite['Pol_Confidence'] = aylien_conf
+    print towrite
+    all_rows.append(towrite)
+
+percentage = correct_p/float(len(pred_sent_col))*100
+print percentage
+df = pd.DataFrame(all_rows)
+df.to_csv("21Feb_Pos.csv")
